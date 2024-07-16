5a52b5456b223319d5a0d9b190de2cdc8162c7e4
leduardodp <ledagostino@outlook.com>
2024-07-16 12:58:27 +0200

    debug true

--- core/settings_prod.py
+++ core/settings_prod.py
@@ -21,7 +21,7 @@ CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # load production server from .env
 ALLOWED_HOSTS  =['*']
